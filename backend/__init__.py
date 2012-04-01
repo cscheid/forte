2c3e344bf3d9feb9258e9dba128f4cee0b8e2588
Carlos Scheidegger <cscheid@research.att.com>
2012-04-01 19:26:28 -0400

    import pywebsocket from http://code.google.com/p/pywebsocket/

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,20 +1,12 @@
-import pygame
-import pygame.midi
+# from midi_reader import KeyReader
+from server import Server
 
-pygame.midi.init()
+# reader = KeyReader()
+# for k in reader.read():
+#     print k
 
-##############################################################################
+# if __name__ == '__init__':
+    
 
-class KeyReader(object):
-    def __init__(self):
-        self._input = pygame.midi.Input(0)
-        self._active = False
-    def read(self):
-        self._active = True
-        while self._active:
-            if self._input.poll():
-                yield self._input.read(1)[0]
-        
-reader = KeyReader()
-for k in reader.read():
-    print k
+s = Server()
+s.serve()
