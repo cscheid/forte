b1aa34a1a6a1aab11a6646215d99862432c95957
Carlos Scheidegger <cscheid@research.att.com>
2012-04-01 17:29:30 -0400

    a piano chart object with keyup-keydown

--- frontend/js/piano.js
+++ frontend/js/piano.js
@@ -1,57 +1,78 @@
-var chart = d3.select("#piano_div").append("svg");
-var chart_width = 1200;
+var chart_width = 960;
 var chart_height = 320;
-chart
-    .attr("width", chart_width)
-    .attr("height", chart_height);
-
 var key_height = 90;
 var data = _.range(21, 109);
 
-//            C   C#   D   D#   E   F   F#    G   G#     A   A# B
-var key_xs = [0, 10, 15, 25, 30, 45, 55, 60, 70, 75.0, 85, 90];
-var key_color = [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0];
-var y_color = [0, 30];
-var key_width = [15, 10];
+function make_chart(div)
+{
+    //                     C  C#   D  D#   E   F  F#   G  G#   A  A#   B
+    var key_xs_index    = [0, 11, 15, 26, 30, 45, 56, 60, 71, 75, 86, 90];
+    var key_color_index = [0,  1,  0,  1,  0,  0,  1,  0,  1,  0,  1,  0];
+    var y_color   = [0, 30];
+    var key_width = [15, 8];
 
-chart.selectAll("rect")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", function(d) { return String("key-") + d; })
-    .attr("x", function(d, i) { return key_xs[d % 12] + (105 * ~~(d / 12)); })
-    .attr("width", function(d) {
-        var color = key_color[d % 12];
-        return key_width[color];
-    })
-    .attr("height", function(d) { 
-        var k = d % 12; 
-        var color = key_color[k];
-        return key_height - y_color[color];
-    })
-    .attr("y", 0)
-    .attr("fill", function(d) {
-        var k = d % 12; 
-        var color = key_color[k];
-        return ["white", "black"][color];
-    })
-    .attr("stroke", function(d) {
-        var k = d % 12; 
-        var color = key_color[k];
-        return ["black", "white"][color];
-    })
-    .sort(function(d1, d2) {
-        var k1 = d1 % 12;
-        var k2 = d2 % 12;
-        var c1 = key_color[k1];
-        var c2 = key_color[k2];
-        if      (c1 < c2)       return -1;
-        else if (c1 > c2)       return 1;
-        else if (d1 < d2)       return -1;
-        else if (d1 > d2)       return 1;
-        else                    return 0;
-//         if (d1 < d2) { return 1; }
-//         else if (d1 > d2) { return -1; }
-//         else return 0;
-    }).order()
-    ;
+    function which_key(d) {
+        return key_color_index[d % 12];
+    }
+    function key_color(d) {
+        return ["white", "black"][which_key(d)];
+    }
+    function which_octave(d) {
+        return ~~(d / 12);
+    }
+    function which_x(d) {
+        var octave = which_octave(d);
+        return key_xs_index[d % 12] + (105 * octave);
+    }
 
+    function init_chart()
+    {
+        var chart = div.append("svg");
+        chart
+            .attr("width", chart_width)
+            .attr("height", chart_height);
+
+        chart.selectAll("rect")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", function(d) { return String("key-") + d; })
+            .attr("x", function(d, i) { 
+                return which_x(d) - which_x(d3.min(data));
+            })
+            .attr("width", function(d) {
+                return key_width[which_key(d)];
+            })
+            .attr("height", function(d) { 
+                return key_height - y_color[which_key(d)];
+            })
+            .attr("y", 0)
+            .attr("fill", key_color)
+            .attr("stroke", "black")
+            .sort(function(d1, d2) {
+                var c1 = which_key(d1), c2 = which_key(d2);
+                if      (c1 < c2)       return -1;
+                else if (c1 > c2)       return 1;
+                else if (d1 < d2)       return -1;
+                else if (d1 > d2)       return 1;
+                else                    return 0;
+            }).order()
+        ;
+        return chart;
+    }
+
+    var chart = init_chart();
+    return {
+        chart: chart,
+        key_down: function(d) {
+            chart.select(".key-" + d).attr("fill", "red");
+        },
+        key_up: function(d) {
+            chart.select(".key-" + d).attr("fill", key_color);
+        }
+    };
+}
+
+var chart = make_chart(d3.select("#piano_div"));
+
+// window.setTimeout(function() { chart.key_down(30); }, 1000);
+// window.setTimeout(function() { chart.key_up(30); }, 2000);
